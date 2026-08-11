e44fac1f36dc24612b0af97777d311ca31fa86e8
Emmet Rohweder <emmet.rohweder@tu.org>
2026-08-11 10:38:31 -0600

    now a iterable list format and changed api fetch to json format to fix geometry issue

--- get_inputs.R
+++ get_inputs.R
@@ -13,52 +13,72 @@ invisible(lapply(list.files("functions", pattern = "\\.R$", full.names = TRUE), 
 # State to query priority waters
 state <- "Wyoming"
 
-# Export ID
-id <- "WY"
+# Export id
+export_id <- "WY_pw"
 
 # ============================================================
-# Get huc12s for priority waters
+# Get huc12s
 # ============================================================
-url <- "https://services1.arcgis.com/754BERmVIq3RqSf8/arcgis/rest/services/TU_National_Priority_Waters_Official_view/FeatureServer/2"
-tu_layer <- arc_open(url)
+base_url = "https://services1.arcgis.com/754BERmVIq3RqSf8/arcgis/rest/services/TU_National_Priority_Waters_Official_view/FeatureServer/2/query"
 
-get_priority_waters <- function(state, pwName = NULL, layer = tu_layer) {
+get_priority_waters <- function(state) {
+  
   where_clause <- sprintf("state = '%s'", state)
   
-  if (!is.null(pwName)) {
-    where_clause <- paste(
-      where_clause,
-      sprintf("AND pwName = '%s'", pwName)
-    )
-  }
+  where_enc <- URLencode(where_clause, reserved = TRUE)
   
-  arc_select(
-    layer,
-    where = where_clause
+  q_url <- sprintf(
+    "%s?where=%s&outFields=*&f=geojson",
+    base_url,
+    where_enc
   )
+  
+  st_read(q_url, quiet = TRUE)
 }
 
-aoi <- get_priority_waters(state) |>
-  st_make_valid() |>
-  st_union()
-  
-huc12s <- get_huc(AOI = aoi, type = "huc12")
+pw <- get_priority_waters(state) |> st_make_valid()
+
+plot(pw$geometry)
+
+unique(pw$pwName)
+
+huc12s_list <- unique(pw$pwName) |>
+  set_names() |>
+  map(function(nm) {
+    aoi <- pw |>
+      filter(pwName == nm) |>
+      st_make_valid() |>
+      st_union()
+    get_huc(AOI = aoi, type = "huc12")
+  })
 
 # ============================================================
 # Get Riverscapes, FlowMet, and NorWeST data
 # ============================================================
-rcat <- get_riverscapes(huc12 = huc12s, project_type = "rcat")
-brat <- get_riverscapes(huc12 = huc12s, project_type = "brat")
-flowmet <- get_flowmet(huc12 = huc12s)
-norwest <- get_norwest(huc12 = huc12s)
+results <- huc12s_list |>
+  imap(function(huc12s, nm) {
+    message("Processing: ", nm)
+    list(
+      huc12s = huc12s,
+      rcat = get_riverscapes(huc12 = huc12s, project_type = "rcat"),
+      brat = get_riverscapes(huc12 = huc12s, project_type = "brat"),
+      flowmet = get_flowmet(huc12 = huc12s),
+      norwest = get_norwest(huc12 = huc12s)
+    )
+  })
 
 # ============================================================
 # Export
 # ============================================================
 dir.create("inputs", showWarnings = FALSE, recursive = TRUE)
 
-write.csv(rcat, file.path("inputs", paste0(id, "_huc12s_rcat.csv")), row.names = FALSE)
-write.csv(brat, file.path("inputs", paste0(id, "_huc12s_brat.csv")), row.names = FALSE)
-write.csv(flowmet, file.path("inputs", paste0(id, "_huc12s_flowmet.csv")), row.names = FALSE)
-write.csv(norwest, file.path("inputs", paste0(id, "_huc12s_norwest.csv")), row.names = FALSE)
-st_write(huc12s, file.path("inputs", paste0(id, "_huc12s.gpkg")), delete_dsn = TRUE)
+iwalk(results, function(res, id) {
+  id <- gsub(" ", "_", id)
+  full_id <- paste0(export_id, "_", id)
+  
+  write.csv(res$rcat, file.path("inputs", paste0(full_id, "_huc12s_rcat.csv")), row.names = FALSE)
+  write.csv(res$brat, file.path("inputs", paste0(full_id, "_huc12s_brat.csv")), row.names = FALSE)
+  write.csv(res$flowmet, file.path("inputs", paste0(full_id, "_huc12s_flowmet.csv")), row.names = FALSE)
+  write.csv(res$norwest, file.path("inputs", paste0(full_id, "_huc12s_norwest.csv")), row.names = FALSE)
+  st_write(res$huc12s, file.path("inputs", paste0(full_id, "_huc12s.gpkg")), delete_dsn = TRUE, quiet = TRUE)
+})
